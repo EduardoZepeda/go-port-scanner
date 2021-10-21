7d9cd99dcb3e4803971c25cc38bf72698940159f
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2021-10-21 13:26:12 -0500

    Add goroutines to port scanner

--- main.go
+++ main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sync"
 )
 
+var site = flag.String("site", "scanme.nmap.org", "url to scan")
+
 func main() {
-	for i := 0; i < 100; i++ {
-		conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", "scanme.nmap.org", i))
-		if err != nil {
-			continue
-		}
-		defer conn.Close()
-		fmt.Printf("Port %d is open\n", i)
+	flag.Parse()
+	var wg sync.WaitGroup
+	fmt.Printf("Scanning %s...\n", *site)
+	for i := 0; i < 65535; i++ {
+		wg.Add(1)
+		go func(port int) {
+			conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", *site, port))
+			if err != nil {
+				return
+			}
+			wg.Done()
+			conn.Close()
+			fmt.Printf("[] Port %d is open\n", port)
+		}(i)
 	}
+	wg.Wait()
 }
